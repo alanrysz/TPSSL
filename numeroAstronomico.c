03852ff65448f7cd706ff7a0b56512e2cc24d8f6
alanrysz <alandvc@gmail.com>
2020-10-17 14:01:23 -0300

    Create numeroAstronomico.c

---
+++ numeroAstronomico.c
@@ -0,0 +1,14 @@
+/*
+* Trabajo Práctico Promoción (Numeros Astronomicos)
+* Integrantes: Agustin Sánchez y Alan Rysznivkier
+* Año: 2020
+*/
+#include <stdio.h>
+#include "numeroAstronomico.h"
+
+int main(){
+
+    menu();
+
+    return 0;
+}
